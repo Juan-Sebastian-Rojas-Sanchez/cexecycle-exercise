4e2aa7dffc606cd3231ccb1ec8ad4e662c24c579
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-11-01 16:15:17 -0500

    Ejercicio Múltiplos

---
+++ main.py
@@ -0,0 +1,9 @@
+#Solicitar al usuario que ingrese un número
+number = int(input("Enter a number to display its multiplication table: "))
+
+#Mostrar la tabla de multiplicar del número ingresado
+print(f"\nMultiplication table for {number}:\n")
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} x {i} = {result}")
+
